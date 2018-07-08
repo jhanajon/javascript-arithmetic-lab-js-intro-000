83a9fc44c242b04c7bea187a677f5984363843dd
joe fisher <jhanajon@gmail.com>
2018-07-08 23:09:32 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,36 @@
+var a, b
+
+function add(a,b) {
+ return a + b;
+}
+
+function subtract(a,b) {
+ return a - b;
+}
+
+function multiply(a,b) {
+ return a * b;
+}
+
+function divide(a,b) {
+ return a / b;
+}
+
+
+function inc(a,b) {
+ return a +1;
+}
+
+function dec(a,b) {
+ return a - 1;
+}
+
+function makeInt(a,n) {
+  a = parseInt(a.toString);
+  n = 10;
+
+}
+{
+  // (makeInt('0x2328')).toEqual(0)
+//  n = 0;
+}
